7dda12f080162ae028f440c96a4e912ac25c9509
Gustavo Procopio <rattesprocopio@gmail.com>
2025-02-06 15:23:36 -0300

    feat: Criação do POST

--- src/test/java/com/compass/microservicoB/repository/PostRepository.java
+++ src/test/java/com/compass/microservicoB/repository/PostRepository.java
@@ -1,4 +1,9 @@
 package com.compass.microservicoB.repository;
 
-public class PostRepository {
+import com.compass.microservicoB.model.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
 }
