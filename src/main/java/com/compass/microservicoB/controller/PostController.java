de37455851d421b9745e5d9342e0f3975831106e
Gustavo Procopio <rattesprocopio@gmail.com>
2025-02-06 15:22:06 -0300

    feat: Criação do POST

---
+++ src/main/java/com/compass/microservicoB/controller/PostController.java
@@ -0,0 +1,55 @@
+package com.compass.microservicoB.controller;
+
+import com.compass.microservicoB.model.Post;
+import com.compass.microservicoB.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+
+    @PostMapping
+    public ResponseEntity<Post> createPost(@RequestBody Post post) {
+        Post createdPost = postService.createPost(post);
+        return ResponseEntity.ok(createdPost);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<Post>> getAllPosts() {
+        List<Post> posts = postService.getAllPosts();
+        return ResponseEntity.ok(posts);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable String id) {
+        Optional<Post> post = postService.getPostById(id);
+        return post.map(ResponseEntity::ok)
+                       .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Post> updatePost(@PathVariable String id, @RequestBody Post postDetails) {
+        Optional<Post> updatedPost = postService.updatePost(id, postDetails);
+        return updatedPost.map(ResponseEntity::ok)
+                       .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable String id) {
+        postService.deletePost(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/test/java/com/compass/microservicoB/model/Post.java
@@ -0,0 +1,4 @@
+package com.compass.microservicoB.model;
+
+public class Post {
+}
