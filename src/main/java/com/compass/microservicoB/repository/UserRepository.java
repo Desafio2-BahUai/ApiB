2a5af59c72aa497b41dead570b76f184bc788ad9
Arthur Heinen Lenhardt <arthur.lenhardt.pb@compasso.com.br>
2025-02-10 12:49:52 -0300

    Criação do UserRepository

---
+++ src/main/java/com/compass/microservicoB/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.compass.microservicoB.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.compass.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
